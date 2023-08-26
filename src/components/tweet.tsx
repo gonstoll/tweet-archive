9627bc52c2778c2374aad1aa3cc41de14a817094
Gonzalo Stoll <stollgonzalo@gmail.com>
2023-08-26 22:41:05 +0200

    Trying out new tweet Api and using EmbeddedTweet

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,15 +1,28 @@
-import {Tweet as ReactTweet} from 'react-tweet'
+import {EmbeddedTweet} from 'react-tweet'
+import {type Tweet} from 'react-tweet/api'
 import {type UserTweet} from '~/db/models/tweet'
 import {Tag} from './tag'
 
-export function Tweet({tweet}: {tweet?: UserTweet}) {
-  return tweet ? (
+async function getTweetData(tweetId: string) {
+  const res = await fetch(`https://react-tweet.vercel.app/api/tweet/${tweetId}`)
+
+  if (res.statusText !== 'OK') {
+    throw new Error('Failed to fetch tweet data')
+  }
+
+  return res.json() as Promise<{data: Tweet}>
+}
+
+export async function Tweet({tweet}: {tweet?: UserTweet}) {
+  const tweetData = tweet ? await getTweetData(tweet.tweetId) : undefined
+
+  return tweet && tweetData ? (
     <div
       key={tweet.id}
       className="mb-4 break-inside-avoid rounded-md border-1 border-slate-200 p-4"
     >
       <div className="mb-4">
-        <ReactTweet id={String(tweet.tweetId)} />
+        <EmbeddedTweet tweet={tweetData.data} />
       </div>
       <div className="flex flex-wrap gap-2">
         {tweet.tags?.map(tag => <Tag key={tag.id} tag={tag} />)}
