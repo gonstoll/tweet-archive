4370733d7b9d06a56d04ae0e0386191d93f927e2
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-10-18 22:39:58 +0200

    More progress on new tweet UI

--- src/components/tweet/tweet-content.tsx
+++ src/components/tweet/tweet-content.tsx
@@ -27,87 +27,92 @@ export function NewTweetContent({tweet, tweetData, deleteTweet}: TweetContent) {
 
   const tweetDate = new Date(tweetData.created_at)
 
-  console.log('logging tweetData', tweetData)
   return (
-    <div className="mb-4 break-inside-avoid rounded-xl border bg-white shadow-md">
-      <div className="flex-column w-full p-8 md:flex">
-        <div className="flex items-center">
-          <Image
-            className="rounded-full"
-            src={tweetData.user.profile_image_url_https}
-            alt={`${tweetData.user.name}'s profile picture`}
-            height="40"
-            style={{
-              aspectRatio: '40/40',
-              objectFit: 'cover',
-            }}
-            width="40"
-          />
-          <div className="ml-4">
-            <p className="text-sm font-semibold uppercase tracking-wide text-black dark:text-white">
-              {tweetData.user.name}
-            </p>
-            <p className="text-gray-400 dark:text-gray-300">
-              @{tweetData.user.screen_name}
-            </p>
-          </div>
-        </div>
-        <p className="mt-4 text-gray-500 dark:text-gray-300">
-          {tweetData.text}
-        </p>
-        <div className="mt-6 flex items-center justify-between">
-          <div className="flex space-x-2 text-gray-400 dark:text-gray-300">
-            <div className="flex items-center">
-              <svg
-                className=" h-6 w-6 text-red-500"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-              </svg>
-              <span className="ml-1 text-red-500">
-                {tweetData.favorite_count}
-              </span>
-            </div>
-            <div className="flex items-center">
-              <svg
-                className=" h-6 w-6 text-green-500"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" />
-              </svg>
-              <span className="ml-1 text-green-500">
-                {tweetData.conversation_count}
-              </span>
+    <div
+      className={classNames('mb-4 break-inside-avoid', {
+        'opacity-50': isPending,
+      })}
+    >
+      <div className="relative rounded-xl border bg-white shadow-md">
+        <div className="flex-column w-full p-8 md:flex">
+          <div className="flex items-center">
+            <Image
+              className="rounded-full"
+              src={tweetData.user.profile_image_url_https}
+              alt={`${tweetData.user.name}'s profile picture`}
+              height="40"
+              style={{
+                aspectRatio: '40/40',
+                objectFit: 'cover',
+              }}
+              width="40"
+            />
+            <div className="ml-4">
+              <p className="text-sm font-semibold uppercase tracking-wide text-black dark:text-white">
+                {tweetData.user.name}
+              </p>
+              <p className="text-gray-400 dark:text-gray-300">
+                @{tweetData.user.screen_name}
+              </p>
             </div>
           </div>
-          <div className="text-gray-400 dark:text-gray-300">
-            {tweetDate.toLocaleDateString('en-US', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-              hour: 'numeric',
-              minute: 'numeric',
-            })}
+          <p className="mt-4 text-gray-500 dark:text-gray-300">
+            {tweetData.text}
+          </p>
+          <div className="mt-6 flex items-center justify-between">
+            <div className="flex space-x-2 text-gray-400 dark:text-gray-300">
+              <div className="flex items-center">
+                <svg
+                  className=" h-6 w-6 text-red-500"
+                  fill="none"
+                  height="24"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+                </svg>
+                <span className="ml-1 text-red-500">
+                  {tweetData.favorite_count}
+                </span>
+              </div>
+              <div className="flex items-center">
+                <svg
+                  className=" h-6 w-6 text-green-500"
+                  fill="none"
+                  height="24"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" />
+                </svg>
+                <span className="ml-1 text-green-500">
+                  {tweetData.conversation_count}
+                </span>
+              </div>
+            </div>
+            <div className="text-gray-400 dark:text-gray-300">
+              {tweetDate.toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+              })}
+            </div>
           </div>
         </div>
       </div>
-      <div className="border-t border-t-slate-300 bg-slate-100 p-8 py-6">
+      <div className="-mt-4 rounded-xl bg-slate-100 p-8 pb-6 pt-10">
         {tweet.tags?.length ? (
           <div className="mb-4 flex flex-wrap gap-2">
             {tweet.tags.map(tag => (
